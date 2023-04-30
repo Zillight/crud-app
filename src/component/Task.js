6737fbbffe0de76a37df8e9ec3d85b750584f34f
Banji Zillight <banjiolawolez@gmail.com>
2023-04-30 20:50:23 +0100

    finessing

--- src/component/Task.js
+++ src/component/Task.js
@@ -2,6 +2,7 @@ export default function Task (props) {
     return (
         <div className='task'>
               <h1>{props.taskName}</h1>
+              <button>✔︎</button>
               <button onClick={()=> props.deleteTask(props.id)}> X </button>
             </div>
     )
